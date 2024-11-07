e71b08b04fb68f87b113b2b97f04039bcbf1217d
Sorel <sorelpoveda2713@gmail.com>
2024-11-06 22:42:11 -0500

    Update Port app.js

--- app.js
+++ app.js
@@ -479,6 +479,8 @@ app.get("/", (req, res) => {
   res.send("API de Mr CarWash & Parking");
 });
 
-app.listen(3000, () => {
-  console.log("Server on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
